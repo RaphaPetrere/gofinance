dd5677775fb9c06f24b123aac2fce1ac1c272c23
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-06 18:35:00 -0300

    ✨ Creating dashboard header with rn-responsive-fontsize

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: 'https://avatars.githubusercontent.com/u/56046074?v=4'}}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Marilene</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+        <Icon name='power' />
+      </Header>
     </Container>
   )
 }

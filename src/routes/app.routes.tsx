cb5971018512920a2384e4fdc941c010eafd6458
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-07 21:47:31 -0300

    ✨ Styling tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 
 import {
   createBottomTabNavigator
@@ -9,19 +12,58 @@ import { Register } from '../screens/Register';
 const {Navigator, Screen} = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const theme = useTheme();
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0
+        }
+      }}
+    >
       <Screen 
         name='Listagem'
         component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons 
+              name='format-list-bulleted' 
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
       <Screen 
         name='Cadastro'
         component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons 
+              name='attach-money' 
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
       <Screen 
         name='Resumo'
         component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons 
+              name='pie-chart' 
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
     </Navigator>
   )

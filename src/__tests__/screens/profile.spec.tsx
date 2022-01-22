3fa0404df565124c6c76f2f5efdeb7f3efbdad02
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-22 12:00:41 -0300

    ✨ Using jest expect and toBeTruthy

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react-native';
 import { Profile } from '../../screens/Profile';
 
 test('check if show correctly user input name placeholder', () => {
-  render(<Profile />);
+  const { getByPlaceholderText } = render(<Profile />);
+  const inputName = getByPlaceholderText('Nome');
+  expect(inputName).toBeTruthy();
 });
